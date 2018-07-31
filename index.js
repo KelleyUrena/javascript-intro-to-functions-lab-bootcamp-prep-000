11b9840524acf3e6346b4d4ca10fa0232e264f6e
kelleynurena@gmail.com Urena <kelleynurena@gmail.com>
2018-07-31 02:21:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,18 +7,28 @@ function whisper(string) {
 }
 
 function sayHiToGrandma(string) {
-if (string == string.toUpperCase()) {
- return ('YES INDEED');
-}
-
-if (string == string.toLowerCaseCase()) {
- return ('I can\'t hear you!');
-}
-
-if (string == mixedCase.toLowerCaseCase()) {
- return ("I love you, too");
-}
-}
-
-
+ 
+  if (string === shout(string)) {
+   return ('YES INDEED!');
+  }
   
+  if (string === whisper(string)) {
+   return ('I can\'t hear you!');
+  }
+  
+  return ("I love you, too.");
+
+}
+
+function logShout(string) {
+  if (string.toUpperCase === string)
+  console.log ("HELLO")
+}
+
+function logWhisper(string) {
+  if (string.toLowerCase === string)
+  console.log ("hello")
+}
+
+
+
